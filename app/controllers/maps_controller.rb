f8ee65315a238ceda0a9289f97a5a123814d7f7d
hisadomi <hb.kmaia93@icloud.com>
2020-09-26 12:59:20 +0900

    gon.spotsデータ取得可能

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,5 +1,9 @@
 class MapsController < ApplicationController
   def index
     @spots = Spot.includes(:city)
+    gon.spots = @spots
+    #テスト用データ
+    gon.test = Spot.includes(:city).find(4).name
+
   end
 end
