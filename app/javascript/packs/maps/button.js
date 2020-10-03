977cc83783fb3180a439bcb5df6c7ceb0527f805
hisadomi <hb.kmaia93@icloud.com>
2020-10-03 09:58:28 +0900

    JavaScriptにて画面にボタン表示可能

--- app/javascript/packs/maps/button.js
+++ app/javascript/packs/maps/button.js
@@ -1,6 +1,20 @@
-console.log(gon.spots)
-const spotButton = document.createElement('input');
-spotButton.type = 'button';
-spotButton.id = `${gon.spots.id}`;
-spotButton.value = gon.spots.name;
-document.body.appendChild(spotButton);
+gon.spots.forEach(spot => {
+  console.log(spot)
+  var spotButton = document.createElement('input');
+  spotButton.type = 'button';
+  spotButton.class = "col-md-3"
+  spotButton.id = spot.id;
+  spotButton.value = spot.name;
+  document.body.appendChild(spotButton);
+});
+
+// const spotClass = document.getElementsByClassName('spotname');
+// console.log(spotClass)
+// const spotName = spotClass[1].textContent;
+// console.log(spotName)
+
+// const spotButton = document.createElement('input');
+// spotButton.type = 'button';
+// spotButton.id = `${gon.spots.id}`;
+// spotButton.value = gon.spots.name;
+// document.body.appendChild(spotButton);
--- app/javascript/packs/maps/onClick.js
+++ app/javascript/packs/maps/onClick.js
@@ -1,9 +1,11 @@
+const { Button } = require("bootstrap");
+
 document.addEventListener('DOMContentLoaded', function () {
   var btns = document.querySelectorAll('.icon-btn');
   for (var i = 0; i < btns.length; i++) {
     btns[i].addEventListener('click', function () {
       alert('クリックされたよ！');
-      console.log(i)
+      console.log(document.spot.value)
       this.style.color = 'blue';
     }, false);
   }
