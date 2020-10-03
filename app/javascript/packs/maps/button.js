2a0748349d54acb6c45c1ef4057bc33d18bd297c
hisadomi <hb.kmaia93@icloud.com>
2020-10-03 21:41:50 +0900

    ボタンの縦配置に成功

--- app/javascript/packs/maps/button.js
+++ app/javascript/packs/maps/button.js
@@ -2,7 +2,7 @@ gon.spots.forEach(spot => {
   console.log(spot)
   var spotButton = document.createElement('input');
   spotButton.type = 'button';
-
+  spotButton.className = 'col-md-3';
   spotButton.id = spot.id;
   spotButton.value = spot.name;
   document.body.appendChild(spotButton);
