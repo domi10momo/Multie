6cc339904ae66c7197040d688f2a96b12cc6eef9
hisadomi <hb.kmaia93@icloud.com>
2020-10-04 17:57:39 +0900

    ボタンの配置位置修正

--- app/javascript/packs/maps/button.js
+++ app/javascript/packs/maps/button.js
@@ -1,8 +1,12 @@
+var div = document.createElement('div');
+div.className = 'col-md-3'
+document.body.appendChild(div);
+
 gon.spots.forEach(spot => {
   var spotButton = document.createElement('input');
   spotButton.type = 'button';
-  spotButton.className = 'col-md-3';
+  //spotButton.className = 'col-md-3';
   spotButton.id = spot.id;
   spotButton.value = spot.name;
-  document.body.appendChild(spotButton);
+  div.appendChild(spotButton);
 });
--- app/javascript/packs/maps/initMap.js
+++ app/javascript/packs/maps/initMap.js
@@ -7,4 +7,19 @@
 //   center: { lat: 37.9053235, lng: 139.0296194 },
 //   zoom: 12.5,
 // });
+// var div = document.createElement('div');
+// div.className = 'col-md-9';
+// div.id = 'map_id';
+// document.body.appendChild(div);
 
+// var map = document.createElement('div');
+// map.setAttribute('id', 'map');
+// document.body.appendChild(map);
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map_id'), {
+    center: { lat: 37.9053235, lng: 139.0296194 },
+    zoom: 12.5,
+  });
+}
