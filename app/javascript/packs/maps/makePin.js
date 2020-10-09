f9f998ce3fbc54cdf4c4a345d3068d0ba5bb2a08
hisadomi <hb.kmaia93@icloud.com>
2020-10-10 07:39:19 +0900

    立てたピンに観光地情報を吹き出しで表示

--- app/javascript/packs/maps/makePin.js
+++ app/javascript/packs/maps/makePin.js
@@ -2,13 +2,20 @@ window.makePin = function (map, spot_name) {
   geocoder.geocode({ 'address': spot_name }, function (results, status) {
     //該当する検索結果がヒットした時に、地図の中心を検索結果の緯度経度に更新する
     if (status == 'OK') {
-      new google.maps.Marker({
+      marker = new google.maps.Marker({
         map: map,
         position: results[0].geometry.location
       });
+      markerInfo(marker, spot_name);
     } else {
       //検索結果が何もなかった場合に表示
       alert('該当する結果がありませんでした：' + status);
     }
   });
+}
+
+function markerInfo(marker, name) {
+  new google.maps.InfoWindow({
+    content: name
+  }).open(marker.getMap(), marker);
 }
