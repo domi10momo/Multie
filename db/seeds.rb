86dd433b9c545064f0a35957d82edd1b01d54476
hisadomi <hb.kmaia93@icloud.com>
2020-10-20 20:28:27 +0900

    city_noカラム追加、シーケンスにてid設定追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,15 @@ puts 'Finished Delete'
 puts 'Insert City'
 connection = ActiveRecord::Base.connection();
 CSV.foreach('db/csv_data/city.csv') do |row|
-  City.create({ id: row[0], name: row[1] })
-  connection.execute("select setval('cities_id_seq', 1)")
+  City.create({ city_no: row[0], name: row[1] })
+  connection.execute("select setval('cities_id_seq', (select max(id) from cities))");
 end
 puts 'Finish City'
 
 puts 'Insert Spot'
 CSV.foreach('db/csv_data/spot.csv') do |row|
   Spot.create({ city_id: row[0], name: row[1] })
-  connection.execute("select setval('spots_id_seq', 1)") 
+  connection.execute("select setval('spots_id_seq', (select max(id) from spots))");
 end
 binding.pry
 puts "Finish Spot"
