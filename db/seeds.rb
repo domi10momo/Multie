997b436563d9f2182bccd4953ebbefd83b80df28
hisadomi <hb.kmaia93@icloud.com>
2020-10-20 20:15:52 +0900

    city.csvにid追加、シーケンスにてid設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,17 @@ City.delete_all
 puts 'Finished Delete'
 
 puts 'Insert City'
+connection = ActiveRecord::Base.connection();
 CSV.foreach('db/csv_data/city.csv') do |row|
-  City.create({ name: row[0] })
+  City.create({ id: row[0], name: row[1] })
+  connection.execute("select setval('cities_id_seq', 1)")
 end
 puts 'Finish City'
 
 puts 'Insert Spot'
 CSV.foreach('db/csv_data/spot.csv') do |row|
-  #puts "#{row[0]}  #{row[1]}"
   Spot.create({ city_id: row[0], name: row[1] })
+  connection.execute("select setval('spots_id_seq', 1)") 
 end
 binding.pry
 puts "Finish Spot"
