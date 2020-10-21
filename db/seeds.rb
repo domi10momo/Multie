234d8fc9312849e9c586c86fff1caa1d99d28a26
hisadomi <hb.kmaia93@icloud.com>
2020-10-22 07:51:59 +0900

    Citiesテーブルにcity_idカラム追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,27 @@
 require 'csv'
 
+def reset_id(table_name)
+  connection = ActiveRecord::Base.connection;
+  connection.execute("select setval('#{table_name}_id_seq',(select max(id) from #{table_name}))");
+end
+
 puts 'Delete Data'
 Spot.delete_all
 City.delete_all
 puts 'Finished Delete'
 
 puts 'Insert City'
-connection = ActiveRecord::Base.connection();
 CSV.foreach('db/csv_data/city.csv') do |row|
-  City.create({ city_no: row[0], name: row[1] })
-  connection.execute("select setval('cities_id_seq', (select max(id) from cities))");
+  City.create({ city_id: row[0], name: row[1] })
 end
+reset_id("cities")
 puts 'Finish City'
 
 puts 'Insert Spot'
 CSV.foreach('db/csv_data/spot.csv') do |row|
   Spot.create({ city_id: row[0], name: row[1] })
-  connection.execute("select setval('spots_id_seq', (select max(id) from spots))");
 end
+reset_id("spots")
 binding.pry
 puts "Finish Spot"
+
